5e74a2070cefd32e76ba66db76d95f21dbbab0ed
jonathan-gateley <jonathan-gateley@codermerlin.codermerlin.com>
2024-01-30 22:09:24 -0600

    got rid of old portfolio and added new images

--- DM/Excercises/Chapter 9/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-target=~/www/Digital\ Portfolio/DM/Propositional Logic.pdf
-source=Propositional Logic.tex
-compiled=Propositional Logic.pdf
-pdflatex "$source"
-cp "$compiled" "$target"
-chmod a+r "$target"
-echo "DONE: $compiled -> $target"
